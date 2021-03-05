2c4addb01a5ac99526dc904faeee79772cffc1fd
kayolera Obadia <kayoobadia@gmail.com>
2021-03-05 17:19:18 +0200

    implementacao de repository nos modelo

---
+++ src/main/java/com/kayolera/bookstore/repository/LivroRepository.java
@@ -0,0 +1,11 @@
+package com.kayolera.bookstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kayolera.bookstore.domain.Livro;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer>{
+
+}
