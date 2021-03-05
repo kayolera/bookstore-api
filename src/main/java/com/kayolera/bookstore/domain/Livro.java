c93a65d95c0750de4fdc915ee820440cf67fa3a3
kayolera Obadia <kayoobadia@gmail.com>
2021-03-05 16:56:51 +0200

    Classes do Dominio

---
+++ src/main/java/com/kayolera/bookstore/domain/Livro.java
@@ -0,0 +1,10 @@
+package com.kayolera.bookstore.domain;
+
+public class Livro {
+
+	private Integer id;
+	private String titulo;
+	private String nome_autor;
+	private String texto;
+	private Categoria categoria;
+}
